e9eda4aa2e15b2cba6cc15fbcd6f6583b034a09e
Brian Lee <blee12@uci.edu>
2020-03-17 20:06:55 -0700

    Testing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './App.css';
-import { Button, TextField } from '@material-ui/core';
+import {Button, TextField} from '@material-ui/core';
+import axios from 'axios';
+
+const url = 'http://localhost:5000';
 
 function App() {
   return (
     <div className="App">
       <TextField id = "textbox" label = "Company Symbol"></TextField>
-      <Button variant = "contained" color = "primary">Quote!</Button>
+      <Button variant = "contained" color = "primary" style = {{marginTop: 10, marginLeft: 10}} onClick = {quote}>Quote!</Button>
     </div>
   );
+}
+
+function quote() {
+  console.log("lmfao you clicked it");
+  axios.get(url + '/quote')
 }
 
 export default App;
---
+++ src/Components/results.js
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+
+export default class results extends Component {
+    
+}
